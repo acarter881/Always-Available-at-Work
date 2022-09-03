4b061413c52c45ecfd07c4f67b8173d961eacb1b
Alex Carter <60497471+acarter881@users.noreply.github.com>
2022-09-03 16:43:39 -0500

    Create non_gui_main.py

---
+++ non_gui_main.py
@@ -0,0 +1,22 @@
+import pyautogui 
+import time   
+
+# Ensures the script doesn't fail when you have the mouse pointer in the top left corner
+pyautogui.FAILSAFE = False  
+
+# The `shift` key is a good key to press, but a different key may be selected (see https://pyautogui.readthedocs.io/en/latest/keyboard.html)
+key = 'shift'
+
+# Number of press repetitions
+presses = 1
+
+# Infinite loop
+while 1:
+    # Seconds between each press
+    time.sleep(3)
+    
+    # Press the `key`
+    pyautogui.press(keys=key, presses=presses) 
+
+    # For informational purposes: this line prints, to the console, the time at which `key` is pressed 
+    print(f"{key.title()} was pressed at {time.strftime('%I:%M:%S %p')}")
